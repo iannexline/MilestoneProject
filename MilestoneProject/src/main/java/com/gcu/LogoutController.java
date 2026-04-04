bae4d1665e7738a170e44c79259620aa00c8be9d
Lourdes Garza <lourdesgarzaalfaro@gmail.com>
2026-04-04 00:17:22 -0700

    Added security and controller classes project documentation

--- MilestoneProject/src/main/java/com/gcu/LogoutController.java
+++ MilestoneProject/src/main/java/com/gcu/LogoutController.java
@@ -1,8 +1,8 @@
 package com.gcu;
 
-import jakarta.servlet.http.HttpSession;
+//import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  *  This controller invalidates the current HTTP session when a user logs out of the application.
@@ -15,10 +15,10 @@ public class LogoutController {
 	 * This method invalidates the current HTTP session and redirects the user back to the login page.
 	 * @param session The current HTTP session containing user login data
 	 * @return redirect to the login page
-	 */
     @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
         return "redirect:/login";
     }
+    */
 }
